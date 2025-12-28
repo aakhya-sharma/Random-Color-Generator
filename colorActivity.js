6256105e9c076ed12a2e79b8cecfdc2d8684c24a
Aakhya Sharma <hey.aakhya@gmail.com>
2025-12-29 03:16:14 +0530

    Adding comments and finishing touches

--- colorActivity.js
+++ colorActivity.js
@@ -3,10 +3,15 @@ let btn = document.querySelector('button');
 let box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
+    // Generating random numbers between the range 0 to 255, both inclusive
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
+
+    // Verifying
     console.log(red, green, blue);
+
+    // Manipulating DOM elements
     h2.innerText = `rgb(${red}, ${green}, ${blue})`;
-    box.style.background = `rgb(${red}, ${green}, ${blue})`;
+    box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 })
